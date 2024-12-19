cbbfc1226b337f1e535372d7654c8d2297036feb
cluster_wraith <69491293+martins0023@users.noreply.github.com>
2024-12-19 03:03:25 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
   theme: {
     extend: {
       colors: {
-        primary: "#102A43 ",
+        primary: "#102A43",
         secondary: "#E1B085",
         tertiary: "#170000",
         "black-100": "#D9895C",
